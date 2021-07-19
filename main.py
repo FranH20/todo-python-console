faee5895222b8f5ff54f52d97a77ee9120891d42
Franklin Huichi <carloshuichi@gmail.com>
2021-07-19 00:21:30 -0500

    add rich python

--- main.py
+++ main.py
@@ -1,95 +1,31 @@
-import os
-def add_todo():
-    #Retrive the comment
-    new_todo = input("Add the new todo: ") 
-    #Open the file
-    with open("todo.txt", "a+", encoding="UTF-8") as f:
-        #Move the cursor to the start of the file
-        f.seek(0)
-        #if file is not empty
-        data = f.read(100)
-        if len(data) > 0:
-            f.write('\n')
-        #Add the word in the file
-        f.write(new_todo)
-    #Close the file
-    f.close()
-    print("")
-    input("Succesfully, press enter to continue.....")
-def read():
-    count = 0
-    with open("todo.txt", "r", encoding="UTF-8") as f:
-        for word in f:
-            count = count + 1
-            print(f'{count}.- {word}')
-    f.close()
+from typing import List
+from rich.console import Console, OverflowMethod
 
-def read_todo():
-    read()
-    print("")
-    input("Succesfully, press enter to continue.....")
-
-def save_the_todo():
-    list_words = []
-    with open("todo.txt", "r", encoding="UTF-8") as f:
-        for word in f:
-            list_words.append(word)
-    f.close()
-    return list_words
-def build_todo(list_words):
-    with open("todo.txt", "w", encoding="UTF-8") as f:
-        for word in list_words:
-            f.write(word)
-    f.close
-    
-def edit_todo():
-    read()
-    print("")
-    number_edit = int(input("What line do you need to edit ?  ")) 
-    list_words = save_the_todo()
-    print(list_words[number_edit - 1])
-    word_edit = input("Edit the description of TODO: ")
-    list_words[number_edit - 1] = word_edit + '\n'
-    build_todo(list_words)
-    input("Succesfully, press enter to continue.....")
-    
-def delete_todo():
-    read()
-    print("")
-    number_edit = int(input("What line do you need to delete ?  ")) 
-    list_words = save_the_todo()
-    list_words.pop(number_edit - 1)
-    build_todo(list_words)
-    input("Succesfully, press enter to continue.....")
+console = Console(width=20)
+blue_console = Console(style="white on blue")
+supercali = "supercalifragilisticexpialidocious"
+overflow_methods: List[OverflowMethod] = ["fold", "crop", "ellipsis"]
 
 def run():
-    op = 0
-    while op != 5:
-        op = 0
-        print(
-            '''
-                MENU TODO
-            1.- Add Todo
-            2.- Read Todo
-            3.- Edit Todo
-            4.- Delete Todo
-            5.- Exit
-            '''
-        )
-        try:
-            op = int(input('Enter a number option .... '))
-        except:
-            print("Enter a number please")
-        print("")
-        if op == 1:
-            add_todo() 
-        if op == 2:
-            read_todo()
-        if op == 3:
-            edit_todo()
-        if op == 4:
-            delete_todo()
-        os.system('clear')
-  
-if __name__ == '__main__':
+    console.print([1, 2, 3])
+    console.print("[blue underline]Looks like a link")
+    console.print(locals())
+    console.print("FOO", style="white on blue")
+    console.log("Hello, World!")
+    console.out("Locals", locals())
+    console.rule("[bold red]Chapter 2")
+    style = "bold white on blue"
+    console.print("Rich", style=style)
+    console.print("Rich", style=style, justify="left")
+    console.print("Rich", style=style, justify="center")
+    console.print("Rich", style=style, justify="right")
+    for overflow in overflow_methods:
+        console.rule(overflow)
+        console.print(supercali, overflow=overflow, style="bold blue")
+        console.print()
+    blue_console.print("I'm blue. Da ba dee da ba di.")
+
+
+
+if __name__ == "__main__":
     run()
